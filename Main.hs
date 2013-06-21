91e6f02264f2623e9af89f186c90e8ae288af94c
Nathan Burgers <nateburgers@gmail.com>
2013-06-21 01:58:35 +0000

    we are now curling all sources and diffs

--- Main.hs
+++ Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings, ExtendedDefaultRules #-}
+
 module Main where
 import Data
 import Parse
@@ -9,20 +11,22 @@ import Control.Monad.Trans
 import Control.Monad.Maybe
 import Network.HTTP
 import Network.URI
---import Development.Shake
-import Shelly
+import Development.Shake
+--import Shelly hiding (get)
 import Debug.Trace              -- get rid of this
-import qualified Data.Text as T
-default (T.Text)
+-- import qualified Data.Text as T
+-- default (T.Text)
 
-rtemsRoot = "http://www.rtems.com/ftp/pub/rtems/SOURCES/"
+rtemsRoot = "http://www.rtems.com/ftp/pub/rtems/SOURCES"
 
 rtemsPage :: String -> String
-rtemsPage s = rtemsRoot ++ s ++ "/"
+rtemsPage s = rtemsRoot ++ "/" ++ s ++ "/"
+--rtemsPage s = intercalate "/" [rtemsRoot, s]
 
 rtemsRoute :: [String] -> String -- Technically produces incorrect urls
-rtemsRoute = foldl addDir rtemsRoot
-    where addDir x xs = x ++ "/" ++ xs
+-- rtemsRoute = foldl' addDir rtemsRoot
+--     where addDir x xs = x ++ xs ++ "/"
+rtemsRoute subdirs = intercalate "/" (rtemsRoot:subdirs)
 
 openURL :: String -> MaybeT IO String
 openURL url = case parseURI url of
@@ -71,10 +75,20 @@ packagesWithDiffs sources diffs = map (findAssoc diffs) sources
               sourceTitle == diffTitle
           diffMatches _ _ = False
 
+-- Shelly script generation
 data RtemsConf = RtemsConf
                { version :: Version
                , packages :: [(Package, Maybe Package)]
                } deriving Show
+
+confSources :: RtemsConf -> [Package]
+confSources = (map fst) . packages
+
+confDiffs :: RtemsConf -> [Package]
+confDiffs = (compactMap snd) . packages
+
+packageRoute :: Version -> Package -> String
+packageRoute v p = rtemsRoute [show v, show p]
 
 -- Build System
 -- packageRules :: Package -> Rules ()
@@ -84,32 +98,29 @@ data RtemsConf = RtemsConf
 -- wildFormat :: TarFormat -> String
 -- wildFormat f = "*." ++ (show f)
 
--- curlRule :: String -> Action ()
--- curlRule out = system' "curl" [out]
+curlPackageRule :: Version -> Package -> Rules ()
+curlPackageRule v p = (show p) *> curlPackage
+    where curlPackage name = system' "curl" ["-o", name, packageRoute v p]
 
--- tarRules :: Rules ()
--- tarRules = do
---   (wildFormat GZ) *> curlRule
---   (wildFormat XZ) *> curlRule
---   (wildFormat BZ2) *> curlRule
+shakeIt :: RtemsConf -> IO ()
+shakeIt conf = shakeArgs shakeOptions $ do
+                 forM_ (confSources conf) $ curlPackageRule $ version conf
+                 forM_ (confDiffs conf) $ curlPackageRule $ version conf
+                 want $ map show $ confSources conf
+                 want $ map show $ confDiffs conf
 
--- shakeIt :: RtemsConf -> IO ()
--- shakeIt conf = shakeArgs shakeOptions $ do
-
--- Shelly shell generation
-
-
-scrapeRtemsConf :: IO [RtemsConf]
-scrapeRtemsConf = do
-  links <- scrape rtemsRoot
+scrapeRtemsRepo :: IO [RtemsConf]
+scrapeRtemsRepo = do
+  links <- scrape $ rtemsPage "/"
   let versions = extractVersionLinks links
   packageLinks <- mapM (scrape . rtemsPage . show) versions 
   let sources = map (maxOfJustGroups readPackage) packageLinks
       diffs = map (maxOfJustGroups readDiff) packageLinks
       confs = map (maxOfJustGroups readRepoConf) packageLinks
-      system = zipWith RtemsConf versions $ zipWith packagesWithDiffs sources diffs
-  return $ system
-
-main = shakeArgs shakeOptions $ do
-         tarRules
+      configuration = zipWith RtemsConf versions $ zipWith packagesWithDiffs sources diffs
+  return $ configuration
          
+main = shakeIt . last =<< scrapeRtemsRepo
+main' = do
+  configurations <- scrapeRtemsRepo
+  forM_ configurations shakeIt
